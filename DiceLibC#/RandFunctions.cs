1a67ae07776fadd0f8a46af717c01d0e35680218
Kreston101 <93973537+Kreston101@users.noreply.github.com>
2024-02-19 11:57:26 +0800

    Lib created and tested

---
+++ DiceLibC#/RandFunctions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceLibC_
+{
+    public class RandFunctions
+    {
+        //coin fip, 50/50, 0 = heads, 1 = tails
+        public int CoinFlip()
+        {
+            Random rand = new Random();
+            int randInt = rand.Next(0,2);
+            return randInt;
+        }
+
+        public int DiceFour()
+        {
+            Random rand = new Random();
+            int randInt = rand.Next(0, 4);
+            return randInt;
+        }
+
+        public int DiceSix()
+        {
+            Random rand = new Random();
+            int randInt = rand.Next(0, 6);
+            return randInt;
+        }
+
+        public int DiceEight()
+        {
+            Random rand = new Random();
+            int randInt = rand.Next(0, 8);
+            return randInt;
+        }
+
+        public int DiceTwelve()
+        {
+            Random rand = new Random();
+            int randInt = rand.Next(0, 12);
+            return randInt;
+        }
+
+        public int DiceTwenty()
+        {
+            Random rand = new Random();
+            int randInt = rand.Next(0, 20);
+            return randInt;
+        }
+
+        public int Dice100()
+        {
+            Random rand = new Random();
+            int randInt = rand.Next(0, 100);
+            return randInt;
+        }
+    }
+}
---
+++ DiceLibC#/Test.cs
@@ -0,0 +1,7 @@
+﻿using DiceLibC_;
+RandFunctions randFunc = new RandFunctions();
+
+for(int i = 0; i < 25; i++)
+{
+    Console.WriteLine(randFunc.Dice100());
+}
